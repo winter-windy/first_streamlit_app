61392ad1dcc034603055f14b8aafbe1ec2b373ae
winter-windy <139485094+winter-windy@users.noreply.github.com>
2023-07-14 14:20:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,7 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("pick some fruits:", list(my_fruit_list.index))
+
+#display the table on the page
+streamlit.dataframe(my_fruit_list)
