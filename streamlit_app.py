6a029ffdb3dcb56c97f43d14df380010cb88c414
winter-windy <139485094+winter-windy@users.noreply.github.com>
2023-07-14 14:09:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,8 @@ import pandas
 streamlit.title('My parents healthy food diner')
 
 streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocked Smoothie')
+streamlit.text('🐔 Hard-boiled Rree Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
